9a46f36a305cb523030aa45c11c69ab2f0a928b1
kishore-3101 <purukiss@gmail.com>
2026-03-08 17:21:25 +0530

    database connections update

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "kishore31012008", 
-  database: "asset_management"
+host: process.env.MYSQLHOST,
+user: process.env.MYSQLUSER,
+password: process.env.MYSQLPASSWORD,
+database: process.env.MYSQLDATABASE,
+port: process.env.MYSQLPORT
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("Database connection failed:", err);
-  } else {
-    console.log("Connected to MySQL database");
-  }
+db.connect((err)=>{
+if(err){
+console.log("Database connection failed:",err);
+}else{
+console.log("Database connected");
+}
 });
 
 module.exports = db;
